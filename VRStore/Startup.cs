dda71831449d26f58abb0d33f21d574f8c4cd029
B <b@mobsoft.mx>
2018-04-16 10:39:03 -0700

    Add project files.

---
+++ VRStore/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(VRStore.Startup))]
+namespace VRStore
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
